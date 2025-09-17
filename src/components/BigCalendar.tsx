0f2c8915b0db7d467ac6aa43696166fb8d99fae1
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-09-17 15:38:29 +0600

    implement student Dashboard

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,22 +1,32 @@
 'use client'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import React from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import React, { useState } from 'react'
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import moment from 'moment'
 import { calendarEvents } from '@/lib/data'
 
 const localizer = momentLocalizer(moment)
 
 const BigCalendar = () => {
+    const [view, setView] = useState<View>(Views.WORK_WEEK)
+
+    const handleViewChange = (selectedView: View) => {
+        setView(selectedView)
+    }
 
     return (
-    <div>
+    <div className='h-screen'>
         <Calendar
       localizer={localizer}
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500, width: '100%' }}
+      views={['work_week','day']}
+      view={view}
+      style={{ height: "100%" }}
+      onView={handleViewChange}
+      min={new Date(2025, 9, 1, 8, 0, 0)}
+      max={new Date(2025, 9, 30, 17, 0, 0)}
     />
     </div>
     )
