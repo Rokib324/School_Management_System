8516775784b962dc58302c58f6e929fc683d6e55
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-09-17 15:44:01 +0600

    implement Teacher Dashboard

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
 import React from "react";
 
 const TeacherPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-3xl font-bold text-gray-800 mb-4'>Teacher Dashboard</h1>
-      <p className='text-gray-600'>Welcome to your teacher portal</p>
+    <div className='p-4 flex-1 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT SIDE */}
+      <div className='w-full xl:w-2/3'>
+        <div className="h-full bg-white rounded-lg p-4">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT SIDE */}
+      <div className='w-full xl:w-1/3 flex flex-col gap-4'>
+        <Announcement />
+      </div>
     </div>
   );
 };
