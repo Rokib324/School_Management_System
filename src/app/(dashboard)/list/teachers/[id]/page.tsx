47448e352ddb803d15ace3ba14431afb8284c45e
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-09-23 12:16:48 +0600

    implement small cards, calender, shortcuts and announcements in teacher profile page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,50 +1,116 @@
 import React from 'react'
 import Image from 'next/image'
+import { MdBloodtype, MdDateRange, MdPhone } from 'react-icons/md'
+import { CiCalendarDate } from 'react-icons/ci'
+import { IoMdMail } from 'react-icons/io'
+import { SlCalender } from 'react-icons/sl'
+import { FaBook, FaBuilding } from 'react-icons/fa'
+import { GrSchedule } from 'react-icons/gr'
+import BigCalendar from '@/components/BigCalendar'
+import Link from 'next/link'
+import AnnouncementsListPage from '../../announcements/page'
 
 const SingleTeacherPage = () => {
   return (
-    <div className='flex-1 p-4 flex flex-col xl:flex-row gap-4'>
+    <div className='flex-1 p-4 flex flex-col gap-4 xl:flex-row'>
         {/* LEFT SIDE */}
-        <div className='w-full xl:w-2/3 bg-red-200'>
+        <div className='w-full xl:w-2/3'>
         {/* TOP */}
         <div className='flex flex-col lg:flex-row gap-4'>
         {/* user Info Card */}
-        <div className='flex-1 flex gap-4 bg-red-500'>
-            <div className='w-1/3 bg-yellow-200 flex items-center justify-center'>
-            <Image src="https://images.pexels.com/photos/19380858/pexels-photo-19380858.jpeg" alt="user" width={100} height={100} />
+        <div className='bg-sky py-6 px-4 rounded-md flex-1 flex gap-4'>
+            <div className='w-1/3'>
+            <Image src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" alt="user" width={140} height={144} className='w-36 h-36 rounded-full object-cover' />
             </div>
-            <div className='w-2/3 bg-red-200'>
-            <h1 className='text-2xl font-bold'>Md. Abdul Rahman</h1>
-            <p className='text-sm text-gray-500'>abdul.rahman@school.edu.bd</p>
+            <div className='w-2/3 flex flex-col justify-between gap-4'>
+            <h1 className='text-xl font-semibold'>Md. Alia Akter</h1>
+            <p className='text-sm text-gray-500'>An Engineering Teacher in Computer Science the University of London.</p>
+            <div className='flex items-center justify-between gap-2 flex-wrap text-xs font font-medium'>
+              <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2'>
+              <MdBloodtype />
+              <span>B+</span>
+              </div>
+              <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2'>
+              <MdDateRange />
+              <span>january2025</span>
+              </div>
+              <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2'>
+              <IoMdMail />
+              <span>alia@gmail.com</span>
+              </div>
+              <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2'>
+              <MdPhone />
+              <span>01712345678</span>
+              </div>
+            </div>
             </div>
         </div>
         {/* small cards */}
-        <div className='flex-1 flex flex-col md:flex-row justify-between items-center bg-blue-200 gap-4 flex-wrap p-4'>
-            <div className='w-full md:w-1/2 bg-green-200'>
-            <h1 className='text-2xl font-bold'>Md. Abdul Rahman</h1>
-            <p className='text-sm text-gray-500'>abdul.rahman@school.edu.bd</p>
+        <div className='flex-1 flex gap-4 justify-between flex-wrap'>
+          {/* CARD */}
+          <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+          <SlCalender />
+              <div className="">
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
             </div>
-            <div className='w-1/2 bg-green-200'>
-            <h1 className='text-2xl font-bold'>Md. Abdul Rahman</h1>
-            <p className='text-sm text-gray-500'>abdul.rahman@school.edu.bd</p>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <FaBuilding />
+              <div className="">
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
             </div>
-            <div className='w-1/2 bg-green-200'>
-            <h1 className='text-2xl font-bold'>Md. Abdul Rahman</h1>
-            <p className='text-sm text-gray-500'>abdul.rahman@school.edu.bd</p>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <FaBook />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
             </div>
-            <div className='w-1/2 bg-green-200'>
-            <h1 className='text-2xl font-bold'>Md. Abdul Rahman</h1>
-            <p className='text-sm text-gray-500'>abdul.rahman@school.edu.bd</p>
+            {/* CARD */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+            <GrSchedule />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
             </div>
-        </div>
+          </div>
         </div>
         {/* Bottom */}
-        <div>Bottom</div>
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Teacher&apos;s Schedule</h1>
+          <BigCalendar />
         </div>
+      </div>
         {/* RIGHT SIDE */}
-        <div className='w-full xl:w-1/3 bg-blue-200'>
-        right
+        <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
         </div>
+        <AnnouncementsListPage />
+      </div>
     </div>
   )
 }
